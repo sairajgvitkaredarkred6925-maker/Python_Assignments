262ea296d4f790d55b9679d7604c7d75f1519011
Sairaj Vitkare <242948387+sairajgvitkaredarkred6925-maker@users.noreply.github.com>
2026-02-03 07:15:02 +0530

    Add files via upload

---
+++ Unit-1/List.py
@@ -0,0 +1,27 @@
+#Write a python program to perform following operation on list 
+
+#Create and access list elements
+
+list_1=['cat','dog','snake','lion','fish']
+print('LIST :',list_1)
+
+print('Accessing element :',list_1[1])
+
+#Add and remove elements 
+
+list_1.append('woodpeacker') 
+list_1.remove('fish')
+
+print('Updated list :',list_1)
+
+#Sort list elements 
+
+list_1.sort()
+
+print('Sorted list :',list_1)
+
+#Reverse list elements 
+
+list_1.reverse()
+
+print('Reversed list :',list_1)
